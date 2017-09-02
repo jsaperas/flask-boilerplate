46fe73c6c33cf7cb8987a31bb530bc140516aff2
jsaperas <saperas.jordi@gmail.com>
2017-09-02 13:18:53 -0500

    pylint adjustments

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+"""
+Main Flask driver. Creates and launches our instance of
+a Flask app.
+"""
+
 from flask import Flask
 from views import blueprint
 
 app = Flask(__name__)
 app.register_blueprint(blueprint)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
--- views.py
+++ views.py
@@ -1,8 +1,14 @@
+"""
+Views.py contains all the flask routes
+"""
 from flask import Blueprint, render_template
 
-blueprint = Blueprint('blueprint',__name__,
-                        template_folder='templates')
+blueprint = Blueprint('blueprint', __name__,
+                      template_folder='templates')
 
 @blueprint.route('/')
 def index():
+    """
+    Returns index.html page.
+    """
     return render_template('index.html')
